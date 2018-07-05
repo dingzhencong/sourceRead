abed5392acd166702e0b7a61679f8093d95c6180
dingzhencong <dingzhencong@gmail.com>
2018-07-05 19:13:09 +0800

    删除文件

--- src/main/java/com/dingzhencong/jdk8/LeetCode.java
+++ src/main/java/com/dingzhencong/jdk8/LeetCode.java
@@ -6,29 +6,5 @@ public class LeetCode {
 
     public static void main(String[] args) {
         LeetCode left = new LeetCode();
-        left.fib(3);
-
-//        System.out.println(left.coinChange(new Integer[]{2}, 3));
     }
-
-    int fib(int n){
-        if (n < 1)
-            return 1;
-        int[] dpTable = new int[n + 1];
-        int sum = helper(dpTable, n);
-        return sum;
-    }
-
-    int helper(int[] dpTabale, int n) {
-        if (n == 1 || n == 2) {
-            return 1;
-        }
-        if (dpTabale[n] != 0) {
-            return dpTabale[n];
-        }
-        dpTabale[n] = helper(dpTabale,n - 1) + helper(dpTabale,n - 2);
-        return dpTabale[n];
-    }
-
-    
 }
